d023c93911b7d2221c1745026ed885d26491a2e8
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2024-06-07 23:00:58 +0200

    common/math: Add cf.vhd

---
+++ common/math/cf.vhd
@@ -0,0 +1,359 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.numeric_std_unsigned.all;
+
+-- This module performs the Continued Fraction calculations.  Once initialized
+-- with the integer N, it will repeatedly output values X and Y, such that
+-- 1) X^2 = Y mod N.
+-- 2) |Y|<2*sqrt(N).
+-- In other words, the number of bits in Y is approximately half that of X and N.
+-- The value of Y is represented as a sign bit in W and an absolute value in P.
+
+-- Specifically, this module calculates a recurrence relation with the
+-- following initialiazation:
+-- p_0 = 1
+-- r_0 = 0
+-- x_0 = 1
+-- p_1 = N - M*M
+-- s_1 = 2*M
+-- w_1 = -1
+-- x_1 = M,
+-- and then for each n>=2:
+-- 1) a_n = s_n/p_n
+-- 2) r_n = s_n-a_n*p_n
+-- 3) s_(n+1) = 2*M - r\_n
+-- 4) p_(n+1) = a_n (r_n - r_(n-1)) + p_(n-1)
+-- 5) w_(n+1) = - w_n
+-- 6) x_(n+1) = a_n x_n + x_(n-1) mod N
+-- Steps 1 and 2 in the recurrence are performed simultaneously using the divmod module.
+-- Steps 4 and 6 are performed simultaneously using the add_mult and amm modules.
+
+entity cf is
+   generic (
+      G_DATA_SIZE : integer
+   );
+   port (
+      clk_i     : in    std_logic;
+      rst_i     : in    std_logic;
+      s_start_i : in    std_logic;
+      s_val_i   : in    std_logic_vector(2 * G_DATA_SIZE - 1 downto 0); -- N
+      m_ready_i : in    std_logic;
+      m_valid_o : out   std_logic;
+      m_res_x_o : out   std_logic_vector(2 * G_DATA_SIZE - 1 downto 0); -- X
+      m_res_p_o : out   std_logic_vector(G_DATA_SIZE - 1 downto 0);     -- |Y|
+      m_res_w_o : out   std_logic                                  -- sign(Y)
+   );
+end entity cf;
+
+architecture synthesis of cf is
+
+   constant C_ZERO : std_logic_vector(G_DATA_SIZE - 1 downto 0) := (others => '0');
+   constant C_ONE  : std_logic_vector(G_DATA_SIZE - 1 downto 0) := to_stdlogicvector(1, G_DATA_SIZE);
+
+   -- State variables
+   type     state_type is (IDLE_ST, SQRT_ST, CALC_AR_ST, CALC_XP_ST);
+   signal   state : state_type;
+
+   signal   val_n     : std_logic_vector(2 * G_DATA_SIZE - 1 downto 0);
+   signal   val_2root : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+
+   signal   p_prev : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   r_prev : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   x_prev : std_logic_vector(2 * G_DATA_SIZE - 1 downto 0);
+
+   signal   p_cur : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   s_cur : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   w_cur : std_logic;
+   signal   x_cur : std_logic_vector(2 * G_DATA_SIZE - 1 downto 0);
+
+   signal   a_cur : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   r_cur : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+
+   signal   p_new : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   s_new : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   w_new : std_logic;
+   signal   x_new : std_logic_vector(2 * G_DATA_SIZE - 1 downto 0);
+
+   -- Signals connected to SQRT module
+   signal   sqrt_s_ready : std_logic;
+   signal   sqrt_s_valid : std_logic;
+   signal   sqrt_s_data  : std_logic_vector(2 * G_DATA_SIZE - 1 downto 0);
+   signal   sqrt_m_ready : std_logic;
+   signal   sqrt_m_valid : std_logic;
+   signal   sqrt_m_res   : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   sqrt_m_diff  : std_logic_vector(G_DATA_SIZE     downto 0);
+
+   -- Signals connected to DIVMOD module
+   signal   divmod_s_ready : std_logic;
+   signal   divmod_s_valid : std_logic;
+   signal   divmod_s_val_n : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   divmod_s_val_d : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   divmod_m_ready : std_logic;
+   signal   divmod_m_valid : std_logic;
+   signal   divmod_m_res_q : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   divmod_m_res_r : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+
+   -- Signals connected to AMM module
+   signal   amm_s_ready : std_logic;
+   signal   amm_s_valid : std_logic;
+   signal   amm_s_val_a : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   amm_s_val_x : std_logic_vector(2 * G_DATA_SIZE - 1 downto 0);
+   signal   amm_s_val_b : std_logic_vector(2 * G_DATA_SIZE - 1 downto 0);
+   signal   amm_s_val_n : std_logic_vector(2 * G_DATA_SIZE - 1 downto 0);
+   signal   amm_m_ready : std_logic;
+   signal   amm_m_valid : std_logic;
+   signal   amm_m_res   : std_logic_vector(2 * G_DATA_SIZE - 1 downto 0);
+
+   -- Signals connected to ADD-MULT module
+   signal   add_mult_s_ready : std_logic;
+   signal   add_mult_s_valid : std_logic;
+   signal   add_mult_s_val_a : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   add_mult_s_val_x : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   add_mult_s_val_b : std_logic_vector(2 * G_DATA_SIZE - 1 downto 0);
+   signal   add_mult_m_ready : std_logic;
+   signal   add_mult_m_valid : std_logic;
+   signal   add_mult_m_res   : std_logic_vector(2 * G_DATA_SIZE - 1 downto 0);
+
+   -- Output signals
+   signal   res_x : std_logic_vector(2 * G_DATA_SIZE - 1 downto 0);
+   signal   res_p : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   res_w : std_logic;
+
+begin
+
+   fsm_proc : process (clk_i)
+   begin
+      if rising_edge(clk_i) then
+         if m_ready_i = '1' then
+            m_valid_o <= '0';
+         end if;
+
+         -- Set default values
+         res_x            <= C_ZERO & C_ZERO;
+         res_p            <= C_ZERO;
+         res_w            <= '0';
+
+         sqrt_s_valid     <= '0';
+         divmod_s_valid   <= '0';
+         amm_s_valid      <= '0';
+         add_mult_s_valid <= '0';
+
+         case state is
+
+            when IDLE_ST =>
+               null;
+
+            when SQRT_ST =>
+               -- Wait until data is ready
+               if sqrt_s_valid = '0' and sqrt_m_valid = '1' then
+                  assert (divmod_s_ready = '1' and amm_s_ready = '1' and add_mult_s_ready = '1') or rst_i = '1';
+                  -- Store input values
+                  val_2root      <= sqrt_m_res(G_DATA_SIZE - 2 downto 0) & '0';
+
+                  -- Let: p_0 = 1, r_0 = 0, x_0 = 1.
+                  p_prev         <= C_ONE;
+                  r_prev         <= C_ZERO;
+                  x_prev         <= C_ZERO & C_ONE;
+
+                  -- Let: p_1 = N - M*M, s_1 = 2*M, w_1 = -1, x_1 = M.
+                  p_cur          <= sqrt_m_diff(G_DATA_SIZE - 1 downto 0);
+                  s_cur          <= sqrt_m_res(G_DATA_SIZE - 2 downto 0) & '0';
+                  w_cur          <= '1';
+                  x_cur          <= C_ZERO & sqrt_m_res;
+
+                  -- Store output values
+                  m_valid_o      <= '1';
+
+                  -- Start calculating a_n and p_n.
+                  divmod_s_valid <= '1';
+                  state          <= CALC_AR_ST;
+               end if;
+
+            when CALC_AR_ST =>
+               if divmod_s_valid = '0' and divmod_m_valid = '1' then
+                  -- Store new values of a_n and r_n
+                  a_cur            <= divmod_m_res_q;
+                  r_cur            <= divmod_m_res_r;
+
+                  -- Start calculating x_(n+1) and p_(n+1).
+                  amm_s_valid      <= '1';
+                  add_mult_s_valid <= '1';
+                  state            <= CALC_XP_ST;
+               end if;
+
+            when CALC_XP_ST =>
+               if amm_s_valid = '0' and amm_m_valid = '1' and add_mult_s_valid = '0' and add_mult_m_valid = '1' and m_ready_i = '1' then
+                  -- Update recursion
+                  s_cur          <= s_new;
+                  p_cur          <= p_new;
+                  w_cur          <= w_new;
+                  x_cur          <= x_new;
+
+                  p_prev         <= p_cur;
+                  r_prev         <= r_cur;
+                  x_prev         <= x_cur;
+
+                  -- Store output values
+                  m_valid_o      <= '1';
+
+                  -- Start calculating a_n and p_n.
+                  divmod_s_valid <= '1';
+                  state          <= CALC_AR_ST;
+               end if;
+
+         end case;
+
+         -- A start command should be processed from any state
+         if s_start_i = '1' then
+            m_valid_o    <= '0';
+            val_n        <= s_val_i;
+            sqrt_s_valid <= '1';
+            state        <= SQRT_ST;
+
+            if s_val_i = 0 then
+               sqrt_s_valid <= '0';
+               state        <= IDLE_ST;
+            end if;
+         end if;
+
+         if rst_i = '1' then
+            m_valid_o    <= '0';
+            sqrt_s_valid <= '0';
+            state        <= SQRT_ST;
+         end if;
+      end if;
+   end process fsm_proc;
+
+   -- Calculate M=floor(sqrt(N)).
+   sqrt_s_data      <= val_n;
+
+   -- Calculate a_n = s_n/p_n and r_n = s_n-a_n*p_n.
+   divmod_s_val_n   <= s_cur;
+   divmod_s_val_d   <= p_cur;
+
+   -- Calculate x_(n+1) = (a_n * x_n + x_(n-1)) mod N.
+   amm_s_val_a      <= a_cur;
+   amm_s_val_x      <= x_cur;
+   amm_s_val_b      <= x_prev;
+   amm_s_val_n      <= val_n;
+   x_new            <= amm_m_res;
+
+   -- Calculate p_(n+1) = p_(n-1) + a_n*[r_n - r_(n-1)].
+   add_mult_s_val_a <= a_cur;
+   add_mult_s_val_x <= r_cur - r_prev;
+   add_mult_s_val_b <= C_ZERO & p_prev;
+   p_new            <= add_mult_m_res(G_DATA_SIZE - 1 downto 0);
+
+   -- Calculate s_(n+1) = 2*M - r_n.
+   s_new            <= val_2root - r_cur;
+
+   -- Calculate w_(n+1) = - w_n.
+   w_new            <= not w_cur;
+
+
+   --------------------
+   -- Instantiate SQRT
+   --------------------
+
+   sqrt_m_ready     <= '1';
+
+   sqrt_inst : entity work.sqrt
+      generic map (
+         G_DATA_SIZE => G_DATA_SIZE
+      )
+      port map (
+         clk_i     => clk_i,
+         rst_i     => rst_i,
+         s_ready_o => sqrt_s_ready,
+         s_valid_i => sqrt_s_valid,
+         s_data_i  => sqrt_s_data,
+         m_ready_i => sqrt_m_ready,
+         m_valid_o => sqrt_m_valid,
+         m_res_o   => sqrt_m_res,
+         m_diff_o  => sqrt_m_diff
+      ); -- sqrt_inst
+
+
+   ----------------------
+   -- Instantiate DIVMOD
+   ----------------------
+
+   divmod_m_ready <= '1';
+
+   divmod_inst : entity work.divmod
+      generic map (
+         G_DATA_SIZE => G_DATA_SIZE
+      )
+      port map (
+         clk_i     => clk_i,
+         rst_i     => rst_i,
+         s_ready_o => divmod_s_ready,
+         s_valid_i => divmod_s_valid,
+         s_val_n_i => divmod_s_val_n,
+         s_val_d_i => divmod_s_val_d,
+         m_ready_i => divmod_m_ready,
+         m_valid_o => divmod_m_valid,
+         m_res_q_o => divmod_m_res_q,
+         m_res_r_o => divmod_m_res_r
+      ); -- divmod_inst
+
+
+   ----------------------
+   -- Instantiate AMM
+   ----------------------
+
+   amm_m_ready <= '1';
+
+   amm_inst : entity work.amm
+      generic map (
+         G_DATA_SIZE => G_DATA_SIZE
+      )
+      port map (
+         clk_i     => clk_i,
+         rst_i     => rst_i,
+         s_ready_o => amm_s_ready,
+         s_valid_i => amm_s_valid,
+         s_val_a_i => amm_s_val_a,
+         s_val_x_i => amm_s_val_x,
+         s_val_b_i => amm_s_val_b,
+         s_val_n_i => amm_s_val_n,
+         m_ready_i => amm_m_ready,
+         m_valid_o => amm_m_valid,
+         m_res_o   => amm_m_res
+      ); -- amm_inst
+
+
+   ------------------------
+   -- Instantiate ADD_MULT
+   ------------------------
+
+   add_mult_m_ready <= '1';
+
+   add_mult_inst : entity work.add_mult
+      generic map (
+         G_DATA_SIZE => G_DATA_SIZE
+      )
+      port map (
+         clk_i     => clk_i,
+         rst_i     => rst_i,
+         s_ready_o => add_mult_s_ready,
+         s_valid_i => add_mult_s_valid,
+         s_val_a_i => add_mult_s_val_a,
+         s_val_x_i => add_mult_s_val_x,
+         s_val_b_i => add_mult_s_val_b,
+         m_ready_i => add_mult_m_ready,
+         m_valid_o => add_mult_m_valid,
+         m_res_o   => add_mult_m_res
+      ); -- add_mult_inst
+
+
+   --------------------------
+   -- Connect output signals
+   --------------------------
+
+   m_res_x_o <= x_cur;
+   m_res_p_o <= p_cur;
+   m_res_w_o <= w_cur;
+
+end architecture synthesis;
+
---
+++ common/sim/tb_cf.vhd
@@ -0,0 +1,335 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.numeric_std_unsigned.all;
+
+-- This is a self-verifying testbench for the Continued Fraction module.
+
+entity tb_cf is
+end entity tb_cf;
+
+architecture simulation of tb_cf is
+
+   constant C_DATA_SIZE : integer    := 72;
+
+   -- Signal to control execution of the testbench.
+   signal   clk          : std_logic := '1';
+   signal   rst          : std_logic := '1';
+   signal   test_running : std_logic := '1';
+
+   -- Signals conected to DUT
+   signal   cf_s_start : std_logic;
+   signal   cf_s_val_n : std_logic_vector(2 * C_DATA_SIZE - 1 downto 0);
+   signal   cf_m_ready : std_logic;
+   signal   cf_m_valid : std_logic;
+   signal   cf_m_res_x : std_logic_vector(2 * C_DATA_SIZE - 1 downto 0);
+   signal   cf_m_res_p : std_logic_vector(C_DATA_SIZE - 1 downto 0);
+   signal   cf_m_res_w : std_logic;
+
+begin
+
+   clk <= test_running and not clk after 5 ns;
+   rst <= '1', '0' after 100 ns;
+
+
+   --------------------------------------------------
+   -- Instantiate DUT
+   --------------------------------------------------
+
+   cf_inst : entity work.cf
+      generic map (
+         G_DATA_SIZE => C_DATA_SIZE
+      )
+      port map (
+         clk_i     => clk,
+         rst_i     => rst,
+         s_start_i => cf_s_start,
+         s_val_i   => cf_s_val_n,
+         m_ready_i => cf_m_ready,
+         m_valid_o => cf_m_valid,
+         m_res_x_o => cf_m_res_x,
+         m_res_p_o => cf_m_res_p,
+         m_res_w_o => cf_m_res_w
+      ); -- cf_inst
+
+
+   --------------------------------------------------
+   -- Main test procedure starts here
+   --------------------------------------------------
+
+   main_test_proc : process
+
+      type     res_type is record
+         x : integer;
+         y : integer;
+      end record res_type;
+
+      type     res_vector_type is array (natural range <>) of res_type;
+
+      -- Verify CF processing
+
+      procedure verify_cf (
+         val_n : integer;
+         res   : res_vector_type
+      ) is
+         variable exp_x_v : std_logic_vector(2 * C_DATA_SIZE - 1 downto 0);
+         variable exp_p_v : std_logic_vector(C_DATA_SIZE - 1 downto 0);
+         variable exp_w_v : std_logic;
+      begin
+         report "Verify CF: N=" & integer'image(val_n);
+
+         cf_s_val_n <= to_stdlogicvector(val_n, 2 * C_DATA_SIZE);
+         cf_s_start <= '1';
+         wait until clk = '1';
+         cf_s_start <= '0';
+         wait until clk = '1';
+
+         for i in 0 to res'length-1 loop
+            report "Verifying response (" & integer'image(res(i).x) &
+                   ", " & integer'image(res(i).y) & ")";
+
+            -- Verify received response is correct
+            while cf_m_valid /= '1' loop
+               wait until clk = '1';
+            end loop;
+
+            exp_x_v := to_stdlogicvector(res(i).x, 2 * C_DATA_SIZE);
+            if res(i).y > 0 then
+               exp_p_v := to_stdlogicvector(res(i).y, C_DATA_SIZE);
+               exp_w_v := '0';
+            else
+               exp_p_v := to_stdlogicvector(-res(i).y, C_DATA_SIZE);
+               exp_w_v := '1';
+            end if;
+            assert cf_m_res_x = exp_x_v and
+                   cf_m_res_p = exp_p_v and
+                   cf_m_res_w = exp_w_v
+               report "Received (" & to_string(cf_m_res_x) & ", " & to_string(cf_m_res_w) & ", " & to_string(cf_m_res_p) & ")";
+
+            while cf_m_valid /= '0' loop
+               wait until clk = '1';
+            end loop;
+
+         --
+         end loop;
+
+      --
+      end procedure verify_cf;
+
+      -- These values are copied from the spread sheet cf.xlsx.
+      constant C_RES2059    : res_vector_type :=
+      (
+         (
+            45,
+            -34
+         ),
+         (
+            91,
+            45
+         ),
+         (
+            136,
+            -35
+         ),
+         (
+            227,
+            54
+         ),
+         (
+            363,
+            -7
+         ),
+         (
+            465,
+            30
+         ),
+         (
+            1293,
+            -59
+         ),
+         (
+            1758,
+            5
+         ),
+         (
+            294,
+            -42
+         ),
+         (
+            287,
+            9
+         ),
+         (
+            818,
+            -51
+         ),
+         (
+            1105,
+            38
+         ),
+         (
+            1923,
+            -35
+         ),
+         (
+            833,
+            6
+         ),
+         (
+            1231,
+            -63
+         ),
+         (
+            5,
+            25
+         )
+      );
+
+      constant C_RES2623    : res_vector_type :=
+      (
+         (
+            51,
+            -22
+         ),
+         (
+            205,
+            57
+         ),
+         (
+            256,
+            -39
+         ),
+         (
+            461,
+            58
+         ),
+         (
+            717,
+            -19
+         ),
+         (
+            706,
+            66
+         ),
+         (
+            1423,
+            -27
+         ),
+         (
+            929,
+            74
+         ),
+         (
+            2352,
+            -3
+         ),
+         (
+            2478,
+            41
+         ),
+         (
+            2062,
+            -39
+         ),
+         (
+            1356,
+            13
+         )
+      );
+
+      constant C_RES3922201 : res_vector_type :=
+      (
+         (
+            1980,
+            -1801
+         ),
+         (
+            3961,
+            717
+         ),
+         (
+            21785,
+            -96
+         ),
+         (
+            897146,
+            307
+         ),
+         (
+            2943135,
+            -3240
+         ),
+         (
+            3840281,
+            489
+         ),
+         (
+            2369695,
+            -685
+         ),
+         (
+            3922153,
+            2304
+         ),
+         (
+            2369647,
+            -1443
+         ),
+         (
+            2369599,
+            2407
+         ),
+         (
+            817045,
+            -376
+         ),
+         (
+            1878602,
+            3217
+         ),
+         (
+            2695647,
+            -453
+         ),
+         (
+            1137126,
+            3000
+         ),
+         (
+            3832773,
+            -655
+         ),
+         (
+            689986,
+            615
+         ),
+         (
+            128287,
+            -1027
+         )
+      );
+   begin
+      -- Wait until reset is complete
+      cf_s_start   <= '0';
+      cf_m_ready   <= '1';
+      wait until rst = '0';
+      wait until clk = '1';
+
+      -- Verify CF
+      verify_cf(2623, C_RES2623);
+      wait for 200 ns;
+
+      verify_cf(2059, C_RES2059);
+      wait for 200 ns;
+
+      verify_cf(3922201, C_RES3922201);
+      wait for 200 ns;
+
+      -- Stop test
+      wait until clk = '1';
+      report "Test completed";
+      test_running <= '0';
+      wait;
+   end process main_test_proc;
+
+end architecture simulation;
+
