c809ea52f3a205f27a72768342f83b7c19751de3
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2024-06-08 21:04:24 +0200

    Add common/math/factor_vect.vhd

---
+++ common/math/factor_vect.vhd
@@ -0,0 +1,158 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.numeric_std_unsigned.all;
+
+-- Input is a number. Output is a vector of indices into primes with odd powers.
+
+entity factor_vect is
+   generic (
+      G_DATA_SIZE   : integer;
+      G_VECTOR_SIZE : integer -- Number of primes to attempt trial division
+   );
+   port (
+      clk_i        : in    std_logic;
+      rst_i        : in    std_logic;
+      s_ready_o    : out   std_logic;
+      s_valid_i    : in    std_logic;
+      s_data_i     : in    std_logic_vector(G_DATA_SIZE - 1 downto 0);
+      m_ready_i    : in    std_logic;
+      m_valid_o    : out   std_logic;
+      m_complete_o : out   std_logic;
+      m_data_o     : out   std_logic_vector(G_VECTOR_SIZE - 1 downto 0)
+   );
+end entity factor_vect;
+
+architecture synthesis of factor_vect is
+
+   pure function log2 (
+      arg : natural
+   ) return natural is
+   begin
+      --
+      for i in 0 to arg loop
+         if (2 ** i) >= arg then
+            return i;
+         end if;
+      end loop;
+
+      return -1;
+   end function log2;
+
+   constant C_PRIME_ADDR_SIZE : natural := log2(G_VECTOR_SIZE);
+   constant C_EXP_SIZE        : natural := 8;
+
+   signal   primes_index : std_logic_vector(C_PRIME_ADDR_SIZE - 1 downto 0);
+   signal   primes_data  : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+
+   signal   divexp_s_ready : std_logic;
+   signal   divexp_s_valid : std_logic;
+   signal   divexp_s_val_n : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   divexp_s_val_d : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   divexp_m_ready : std_logic;
+   signal   divexp_m_valid : std_logic;
+   signal   divexp_m_quot  : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+   signal   divexp_m_exp   : std_logic_vector(C_EXP_SIZE - 1 downto 0);
+
+   type     state_type is (IDLE_ST, READ_PRIME_ST, DIVEXP_ST, WAIT_ST);
+   signal   state  : state_type;
+   signal   s_data : std_logic_vector(G_DATA_SIZE - 1 downto 0);
+
+begin
+
+   s_ready_o <= '1' when state = IDLE_ST else
+                '0';
+
+   state_proc : process (clk_i)
+   begin
+      if rising_edge(clk_i) then
+         divexp_s_valid <= '0';
+         if m_ready_i = '1' then
+            m_valid_o <= '0';
+         end if;
+
+         case state is
+
+            when IDLE_ST =>
+               if s_valid_i = '1' and s_ready_o = '1' then
+                  m_data_o     <= (others => '0');
+                  s_data       <= s_data_i;
+                  primes_index <= (others => '0');
+                  state        <= READ_PRIME_ST;
+               end if;
+
+            when READ_PRIME_ST =>
+               state <= DIVEXP_ST;
+
+            when DIVEXP_ST =>
+               divexp_s_val_n <= s_data;
+               divexp_s_val_d <= primes_data;
+               divexp_s_valid <= '1';
+               state          <= WAIT_ST;
+
+            when WAIT_ST =>
+               if divexp_m_valid = '1' then
+                  if and (divexp_m_exp) = '1' then
+                     m_complete_o <= '0';
+                     m_valid_o    <= '1';
+                     state        <= IDLE_ST;
+                  else
+                     s_data                             <= divexp_m_quot;
+                     m_data_o(to_integer(primes_index)) <= divexp_m_exp(0);
+                     if divexp_m_quot = 1 then
+                        m_complete_o <= '1';
+                        m_valid_o    <= '1';
+                        state        <= IDLE_ST;
+                     elsif and (primes_index) = '1' then
+                        m_complete_o <= '0';
+                        m_valid_o    <= '1';
+                        state        <= IDLE_ST;
+                     else
+                        primes_index <= primes_index + 1;
+                        state        <= READ_PRIME_ST;
+                     end if;
+                  end if;
+               end if;
+
+         end case;
+
+         if rst_i = '1' then
+            m_valid_o <= '0';
+            state     <= IDLE_ST;
+         end if;
+      end if;
+   end process state_proc;
+
+   primes_inst : entity work.primes
+      generic map (
+         G_ADDR_SIZE => C_PRIME_ADDR_SIZE,
+         G_DATA_SIZE => G_DATA_SIZE
+      )
+      port map (
+         clk_i   => clk_i,
+         rst_i   => rst_i,
+         index_i => primes_index,
+         data_o  => primes_data
+      ); -- primes_inst
+
+   divexp_m_ready <= m_ready_i;
+
+   divexp_inst : entity work.divexp
+      generic map (
+         G_DATA_SIZE => G_DATA_SIZE,
+         G_EXP_SIZE  => C_EXP_SIZE
+      )
+      port map (
+         clk_i     => clk_i,
+         rst_i     => rst_i,
+         s_ready_o => divexp_s_ready,
+         s_valid_i => divexp_s_valid,
+         s_val_n_i => divexp_s_val_n,
+         s_val_d_i => divexp_s_val_d,
+         m_ready_i => divexp_m_ready,
+         m_valid_o => divexp_m_valid,
+         m_quot_o  => divexp_m_quot,
+         m_exp_o   => divexp_m_exp
+      ); -- divexp_inst
+
+end architecture synthesis;
+
---
+++ common/sim/tb_factor_vect.vhd
@@ -0,0 +1,130 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.numeric_std_unsigned.all;
+
+entity tb_factor_vect is
+end entity tb_factor_vect;
+
+architecture simulation of tb_factor_vect is
+
+   constant C_DATA_SIZE   : integer  :=  8;
+   constant C_VECTOR_SIZE : integer  :=  8;
+
+   signal   test_running : std_logic := '1';
+   signal   clk          : std_logic := '1';
+   signal   rst          : std_logic := '1';
+
+   -- Signals conected to DUT
+   signal   dut_s_ready    : std_logic;
+   signal   dut_s_valid    : std_logic;
+   signal   dut_s_data     : std_logic_vector(C_DATA_SIZE - 1 downto 0);
+   signal   dut_m_ready    : std_logic;
+   signal   dut_m_valid    : std_logic;
+   signal   dut_m_complete : std_logic;
+   signal   dut_m_data     : std_logic_vector(C_VECTOR_SIZE - 1 downto 0);
+
+begin
+
+   clk <= test_running and not clk after 5 ns;
+   rst <= '1', '0' after 100 ns;
+
+
+   --------------------------------------------------
+   -- Instantiate DUT
+   --------------------------------------------------
+
+   factor_vect_inst : entity work.factor_vect
+      generic map (
+         G_DATA_SIZE   => C_DATA_SIZE,
+         G_VECTOR_SIZE => C_VECTOR_SIZE
+      )
+      port map (
+         clk_i        => clk,
+         rst_i        => rst,
+         s_ready_o    => dut_s_ready,
+         s_valid_i    => dut_s_valid,
+         s_data_i     => dut_s_data,
+         m_ready_i    => dut_m_ready,
+         m_valid_o    => dut_m_valid,
+         m_complete_o => dut_m_complete,
+         m_data_o     => dut_m_data
+      ); -- factor_vect_inst
+
+
+   --------------------------------------------------
+   -- Main test procedure starts here
+   --------------------------------------------------
+
+   main_test_proc : process
+      procedure verify (
+         arg      : natural;
+         res      : std_logic_vector;
+         complete : std_logic
+      ) is
+      begin
+         report "verify " & to_string(arg);
+         dut_s_data  <= to_stdlogicvector(arg, C_DATA_SIZE);
+         dut_s_valid <= '1';
+         wait until clk = '1';
+         dut_s_valid <= '0';
+
+         while dut_m_valid = '0' loop
+            wait until clk = '1';
+         end loop;
+
+         assert dut_m_data = res;
+         assert dut_m_complete = complete;
+      end procedure verify;
+
+   begin
+      -- Wait until reset is complete
+      dut_s_valid  <= '0';
+      dut_m_ready  <= '1';
+      wait until rst = '0';
+      wait until clk = '1';
+
+      verify(  2, "00000001", '1');
+      verify(  3, "00000010", '1');
+      verify(  5, "00000100", '1');
+      verify(  7, "00001000", '1');
+      verify( 11, "00010000", '1');
+      verify( 13, "00100000", '1');
+      verify( 17, "01000000", '1');
+      verify( 19, "10000000", '1');
+
+      verify(  4, "00000000", '1'); -- 2^2
+      verify(  8, "00000001", '1'); -- 2^3
+      verify( 16, "00000000", '1'); -- 2^4
+      verify( 32, "00000001", '1'); -- 2^5
+      verify( 64, "00000000", '1'); -- 2^6
+      verify(128, "00000001", '1'); -- 2^7
+
+      verify(  9, "00000000", '1'); -- 3^2
+      verify( 27, "00000010", '1'); -- 3^3
+      verify( 81, "00000000", '1'); -- 3^4
+      verify(243, "00000010", '1'); -- 3^5
+
+      verify(  6, "00000011", '1'); -- 2*3
+      verify( 15, "00000110", '1'); -- 3*5
+      verify( 35, "00001100", '1'); -- 5*7
+      verify( 77, "00011000", '1'); -- 7*11
+      verify(143, "00110000", '1'); -- 11*13
+      verify(221, "01100000", '1'); -- 13*17
+
+      verify( 10, "00000101", '1'); -- 2*5
+      verify( 20, "00000100", '1'); -- 4*5
+      verify( 30, "00000111", '1'); -- 2*3*5
+      verify( 40, "00000101", '1'); -- 8*5
+      verify( 50, "00000001", '1'); -- 2*25
+
+      wait for 20 us;
+
+      -- Stop test
+      wait until clk = '1';
+      report "Test completed";
+      test_running <= '0';
+      wait;
+   end process main_test_proc;
+
+end architecture simulation;
+
